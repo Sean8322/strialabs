14577669570135d39e195bf96ff18962994262da
al-ge <webmatrixa@gmail.com>
2019-03-28 20:02:43 -0700

    animations

---
+++ animations.js
@@ -0,0 +1,24 @@
+// must be defined after the html
+
+ScrollReveal().reveal('.space_text', { delay: 100, distance: '50px', duration: 1200, reset: true});
+
+
+// special
+ScrollReveal().reveal('#box_2', { delay: 100, distance: '10%', origin: 'right', duration: 2500, reset: true});
+ScrollReveal().reveal('#blue_line_1', { delay: 100, distance: '100px', duration: 2000, reset: true});
+ScrollReveal().reveal('.white_image', { delay: 100, distance: '50px', origin: 'top', duration: 2000, reset: true});
+
+ScrollReveal().reveal('#stria_meeting_photo', { delay: 100, distance: '20%', origin: 'right', duration: 2000, reset: true});
+ScrollReveal().reveal('.friends_img', { delay: 100, distance: '30%', origin: 'right', duration: 2000, reset: true});
+ScrollReveal().reveal('.fadein', { delay: 100, distance: '30%', origin: 'right', duration: 2000, reset: true});
+ScrollReveal().reveal('.read_more', { delay: 100, duration: 3000, reset: true})
+
+ScrollReveal().reveal('#horizontal', { delay: 100, distance: '50%', origin: 'right', duration: 2000, reset: true});
+ScrollReveal().reveal('#horizontal_line_3', { delay: 100, distance: '20%', origin: 'right', duration: 2000, reset: true});
+ScrollReveal().reveal('#vertical', { delay: 700, distance: '40%', origin: 'bottom', duration: 2000, reset: true});
+ScrollReveal().reveal('#horizontal_line_2', { delay: 100, distance: '50%', origin: 'left', duration: 2000, reset: true});
+
+ScrollReveal().reveal('.product', { delay: 300, duration: 1200, reset: true});
+
+ScrollReveal().reveal('.info_box', { delay: 100, distance: '50%', origin: 'right', duration: 2000, reset: true, mobile: false});
+ScrollReveal().reveal('.buttonanimate', { delay: 100, distance: '100px', origin: 'left', duration: 2000, reset: true});
