debe615ae55d1614483f76c78579a3f83d675457
al-ge <31600858+al-ge@users.noreply.github.com>
2020-05-04 16:07:58 -0700

    animations
    animations

--- animations.js
+++ animations.js
@@ -4,6 +4,7 @@ ScrollReveal().reveal('.space_text', { delay: 100, distance: '50px', duration: 1
 
 
 // special
+ScrollReveal().reveal('#title_line', { delay: 0, distance: '30%', origin: 'top', duration: 2000, reset: true});
 ScrollReveal().reveal('#box_2', { delay: 100, distance: '10%', origin: 'right', duration: 2500, reset: true});
 ScrollReveal().reveal('#blue_line_1', { delay: 100, distance: '100px', duration: 2000, reset: true});
 ScrollReveal().reveal('.white_image', { delay: 100, distance: '50px', origin: 'top', duration: 2000, reset: true});
@@ -22,3 +23,12 @@ ScrollReveal().reveal('.product', { delay: 300, duration: 1200, reset: true});
 
 ScrollReveal().reveal('.info_box', { delay: 100, distance: '50%', origin: 'right', duration: 2000, reset: true, mobile: false});
 ScrollReveal().reveal('.buttonanimate', { delay: 100, distance: '100px', origin: 'left', duration: 2000, reset: true});
+
+ScrollReveal().reveal('#whiteboard', { delay: 100, distance: '100px', origin: 'left', duration: 1200, reset: true});
+ScrollReveal().reveal('.block img', { delay: 200, distance: '150px', duration: 1500, reset: true});
+
+ScrollReveal().reveal('.contact_us_blue_line', { delay: 0, distance: '30%', origin: 'top', duration: 2000, reset: true});
+ScrollReveal().reveal('#team_photo', { delay: 100, distance: '100px', origin: 'left', duration: 1200, reset: true});
+
+ScrollReveal().reveal('.product', { delay: 100, distance: '100px', origin: 'bottom', duration: 2000, reset: false});
+ScrollReveal().reveal('.member', { delay: 100, distance: '100px', origin: 'left', duration: 2000, reset: false});
